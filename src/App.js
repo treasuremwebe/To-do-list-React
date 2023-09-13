0ad203278cbeb7628de9fbe1b5a265ff7fbd111d
Treasure Mwebe <111517163+treasuremwebe@users.noreply.github.com>
2023-09-13 17:28:00 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+
+function App() {
+
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState(""); 
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo
+    }
+
+    //add the todo item to the list
+    setList([...list, newTodo]);
+
+    //clear input box
+    setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    //filter out todo with the id
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList(newList);
+  };
+
+  return (
+    <div>
+      <h1>ToDo List:</h1>
+      <input
+      type="text"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button onClick={() => addTodo(input)}>Add</button>
+
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
